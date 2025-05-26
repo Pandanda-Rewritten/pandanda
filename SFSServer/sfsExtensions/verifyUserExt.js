38c7cbe1130433f8a012fcf1d7da8fcb2bbf7960
pandandatribute <antoniah@zohomail.eu>
2025-05-26 23:32:12 +0300

    Update verifyUserExt.js

--- SFSServer/sfsExtensions/verifyUserExt.js
+++ SFSServer/sfsExtensions/verifyUserExt.js
@@ -1,8 +1,15 @@
-﻿eval(_server.readFile("utils/eventListener.js"));
+eval(_server.readFile("utils/eventListener.js"));
 eval(_server.readFile("utils/functions.js"));
 eval(_server.readFile("utils/json.js"));
 
-function handlePandandaPacket(cmd, params, user, fromRoom) {}
+var dbase;
+var handlers = {};
+
+function handlePandandaPacket(cmd, params, user, fromRoom) {
+  
+  if (handlers[cmd] == null) {
+  }
+}
 
 function handleLoginVerify(evtObj, user) {
   var username = evtObj.name;
@@ -82,9 +89,89 @@ function handleLoadClient(params, user) {
   );
 }
 
+function handleChangePassword(evtObj, user) {
+  
+  var username = evtObj.name;
+  var oldPassword = evtObj.oldPW;
+  var newPassword = evtObj.newPW;
+  var chan = [user];
+  
+  if (!newPassword || newPassword.length < 5) {
+    return sendResponse(chan, "cp", false);
+  }
+  
+  var qRes = dbase.executeQuery(
+    "SELECT * FROM users WHERE username='" +
+      _server.escapeQuotes(username) +
+      "' LIMIT 1;"
+  );
+  
+  if (qRes != null && qRes.size() > 0) {
+    var storedHash = qRes.get(0).getItem("password");
+    var inputHash = _server.md5(oldPassword);
+    
+    if (storedHash != inputHash) {
+      return sendResponse(chan, "cp", false);
+    }
+    
+    var updateResult = dbase.executeCommand(
+      "UPDATE users SET password='" +
+      _server.md5(newPassword) +
+      "' WHERE username='" +
+      _server.escapeQuotes(username) +
+      "';"
+    );
+    
+    if (updateResult) {
+      sendResponse(chan, "cp", true);
+    } else {
+      sendResponse(chan, "cp", false);
+    }
+  } else {
+    sendResponse(chan, "cp", false);
+  }
+}
+
+function sendResponse(recipients, cmd, isSuccess) {
+  _server.sendResponse(
+    {
+      _cmd: cmd,
+      isSuccess: isSuccess
+    },
+    -1,
+    null,
+    recipients,
+    "xml"
+  );
+}
+
+function sendLoginError(recipients, errorType, errorMsg, additionalData) {
+  var resp = {
+    _cmd: "loginFail",
+    error: errorType,
+    message: errorMsg
+  };
+  
+  if (additionalData) {
+    for (var key in additionalData) {
+      resp[key] = additionalData[key];
+    }
+  }
+  
+  _server.sendResponse(
+    resp,
+    -1,
+    null,
+    recipients,
+    "xml"
+  );
+}
+
 function init() {
+  handlers = {};
   handlers["verify"] = handleLoginVerify;
   handlers["loadClient"] = handleLoadClient;
+  handlers["cp"] = handleChangePassword;
   dbase = _server.getDatabaseManager();
 }
 
